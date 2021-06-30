0e9c313bfc6b259d43eda3a3b6ce8a2bdac01a99
MLGodfrey <mlgodfrey@miners.utep.edu>
2021-06-30 13:25:01 -0600

    History working

---
+++ src/history.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
+#include "history.h"
+
+List *init_history(){
+  List *user_history = (List*) malloc(sizeof(Item)*100);
+  return user_history;
+}
+
+void add_history(List *list, char *str){
+  Item *node = list->root;
+
+  if (node == NULL) { //base case
+    node = (Item*) malloc(sizeof(Item));
+    node->id=1;
+    node->str=str;
+    node->next=NULL;
+    list->root=node;
+    printf("%s",node->str);
+    printf("%i\n",node->id);
+  }else{
+    int id=2;
+    while(node->next != NULL){
+      node = node->next;
+      id++;
+    }
+    node->next = (Item*)malloc(sizeof(Item));
+    node->next->id=id;
+    node->next->str=str;
+    node->next->next=NULL;
+    printf("%s\n",node->next->str);
+    printf("%i\n",node->next->id);
+  }
+}
+
+char *get_history(List *list, int id){
+  Item *node = list->root;
+
+  while(node!=NULL){
+    if(node->id == id){
+      return node->str;
+    }
+    node = node->next;
+  }
+  return "Error";
+}
+
+
+void print_history(List *list){
+  Item *node = list->root;
+  while(node != NULL){
+    printf("\n%s",node->str);
+    node=node->next;
+  }
+}
+void free_history(List *list){
+  Item *node = list->root;
+  while(node != NULL){
+    free(node);
+    node = node->next;
+  }
+  free(list);
+}
+
--- src/uimain.c
+++ src/uimain.c
@@ -4,14 +4,33 @@
 #include "history.h"
 
 int main(){
+  List *history = init_history();
+  while(1){
+    
   printf("\n$ ");
   char user_input[101];
   fgets(user_input, 101, stdin);
+  printf("%s\n", user_input);
 
-  char* start = word_start(user_input);
-  printf("\n%s", "Word start running : \n");
-  printf("%c\n", *start);
+  if(user_input[0] == 33 && (user_input[1] > 47 && user_input[1] < 58)){
+    printf("%s\n", "finding history");
+    int num = atoi(user_input+1);
+    char* token = get_history(history, num);
 
-  
+    if(*token){
+      printf("%s%s\n", "$", token);
+    }
+  }else
+    if(user_input[0] == 33 && user_input[1] == 112){
+      printf("%s", "printing history");
+      print_history(history);
+    }else{
+      char **tokens = tokenize(user_input);
+      print_tokens(tokens);
+      printf("%s\n", "making history");
+      char *copy = copy_str(user_input, str_length(user_input));
+      printf("%s\n", copy);
+      add_history(history,copy);
+    }
+  }
 }
-
