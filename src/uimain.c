cca4a1db7ea96f6e9d5c96a03163ced0570e7316
MLGodfrey <mlgodfrey@miners.utep.edu>
2021-07-01 16:27:54 -0600

    Improved uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -5,15 +5,24 @@
 
 int main(){
   List *history = init_history();
-  while(1){
+  int run =1;
+
+  printf("%s\n","Enter a string to tokenize, or !# (# = 1-9) to recall history, or !p to print history, or !q to quit.");
+  
+  while(run){
     
   printf("\n$ ");
   char user_input[101];
   fgets(user_input, 101, stdin);
   printf("%s\n", user_input);
 
+  if(user_input[0] == 33 && user_input[1] == 113){
+    printf("%s,\n", "--quitting");
+    run = 0;
+  }
+  else
   if(user_input[0] == 33 && (user_input[1] > 47 && user_input[1] < 58)){
-    printf("%s\n", "finding history");
+    printf("%s\n", "--Finding history");
     int num = atoi(user_input+1);
     char* token = get_history(history, num);
 
@@ -22,12 +31,12 @@ int main(){
     }
   }else
     if(user_input[0] == 33 && user_input[1] == 112){
-      printf("%s", "printing history");
+      printf("%s", "--Printing history");
       print_history(history);
     }else{
       char **tokens = tokenize(user_input);
       print_tokens(tokens);
-      printf("%s\n", "making history");
+      printf("%s\n", "--Making history");
       char *copy = copy_str(user_input, str_length(user_input));
       printf("%s\n", copy);
       add_history(history,copy);
